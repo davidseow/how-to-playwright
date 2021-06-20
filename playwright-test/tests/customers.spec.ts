45a935de2aa24307c10be72b330b0e8266f14472
David Seow <david.seow@gmail.com>
2021-06-20 21:21:37 +0100

    test: added couple more tests

---
+++ playwright-test/tests/customers.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, chromium } from "@playwright/test";
+test.use({ storageState: "state.json" });
+
+const { beforeEach, describe } = test;
+const { BASEURL } = process.env;
+
+describe("Customers", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto(`${BASEURL}/react-admin-demo/#/customers`, {
+      waitUntil: "networkidle",
+    });
+  });
+
+  test("should display a list of customers", async ({ page }) => {
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe("Customers");
+
+    const reviewList = await page.$$("#main-content .list-page table tbody tr");
+    expect(reviewList.length).toBe(25);
+  });
+
+  test("should be able to add new customer", async ({ page }) => {
+    const customer = {
+      first_name: "TestFirstName",
+      last_name: "TestLastName",
+      email: "test@email.com",
+    };
+
+    await page.click('a[aria-label="Create"]');
+    for (const property in customer) {
+      await page.fill(`#${property}`, customer[property]);
+    }
+    await page.click('button[aria-label="Save"]');
+
+    await page.waitForURL('**/#/customers/9*');
+  });
+});
---
+++ playwright-test/tests/reviews.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, chromium } from "@playwright/test";
+test.use({ storageState: "state.json" });
+
+const { beforeEach, describe } = test;
+const { BASEURL } = process.env;
+
+describe("Reviews", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto(`${BASEURL}/react-admin-demo/#/reviews`, {
+      waitUntil: "networkidle",
+    });
+  });
+
+  test("should display a list of reviews", async ({ page }) => {
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe("Reviews");
+
+    const reviewList = await page.$$("#main-content .list-page table tbody tr");
+    expect(reviewList.length).toBe(25);
+  });
+});
