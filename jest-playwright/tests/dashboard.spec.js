a4f0b422a4a49b10ff87dfcb254eca1eaa5792fa
David Seow <david.seow@gmail.com>
2021-06-20 16:17:01 +0100

    test: replace test

---
+++ jest-playwright/tests/dashboard.spec.js
@@ -0,0 +1,25 @@
+const { BASEURL } = process.env;
+
+describe("Dashboard", () => {
+  beforeAll(async () => {
+    // TODO: run this once before all test and reuse context
+    await page.goto(`${BASEURL}/react-admin-demo/#/`, {
+      waitUntil: "networkidle",
+    });
+
+    // login
+    await page.fill('input[name="username"]', "demo");
+    await page.fill('input[name="password"]', "demo");
+    await page.click('text="Sign in"');
+
+    const profileHeader = await page.$('[aria-label="Profile"]');
+    expect(await profileHeader.textContent()).toContain("Jane Doe");
+  });
+
+  it("should display dashboard", async () => {
+    const pageCard = await page.$("#main-content .MuiPaper-elevation1");
+    expect(await pageCard.textContent()).toContain(
+      "Welcome to the react-admin e-commerce demo"
+    );
+  });
+});
---
+++ jest-playwright/tests/login.spec.js
@@ -0,0 +1,18 @@
+const { BASEURL } = process.env;
+
+describe("Login", () => {
+  beforeAll(async () => {
+    await page.goto(`${BASEURL}/react-admin-demo`, {
+      waitUntil: "networkidle",
+    });
+  });
+
+  it("should be able to login", async () => {
+    await page.fill('input[name="username"]', "demo");
+    await page.fill('input[name="password"]', "demo");
+    await page.click('text="Sign in"');
+
+    const profileHeader = await page.$('[aria-label="Profile"]');
+    expect(await profileHeader.textContent()).toContain("Jane Doe");
+  });
+});
