eb1b03f7c6f3b458b8b2a6539227961c83db4581
David Seow <david.seow@gmail.com>
2022-08-16 08:24:37 +0100

    test: add fullscreen capture

--- playwright-test/fixtures/base-fixture.ts
+++ playwright-test/fixtures/base-fixture.ts
@@ -1,7 +1,24 @@
 import { test as base } from "@playwright/test";
 
-const test = base.extend<{}>({
-  screenshot: "only-on-failure",
+const test = base.extend<{
+  captureFullScreenOnFail: void;
+}>({
+  screenshot: "off",
+  captureFullScreenOnFail: [
+    async ({ page }, use, testInfo) => {
+      await use(); // run test steps..
+
+      // capture fullscreen if test fails
+      if (testInfo.status === "failed") {
+        const screenshotFileName = `${testInfo.title.replace(/ /g, "_").toLowerCase()}.png`;
+        await page.screenshot({
+          path: `${testInfo.outputDir}/${screenshotFileName}`,
+          fullPage: true,
+        });
+      }
+    },
+    { auto: true },
+  ],
 });
 
 export default test;
