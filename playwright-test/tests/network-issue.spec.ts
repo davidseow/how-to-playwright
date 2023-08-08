77bb3d793ebd76e55c75a38832f38baab826d37a
David Seow <david.seow@gmail.com>
2023-08-08 21:59:11 +0100

    test: update selector

--- playwright-test/tests/network-issue.spec.ts
+++ playwright-test/tests/network-issue.spec.ts
@@ -4,7 +4,7 @@ test.use({ storageState: "state.json" });
 
 const { expect } = test;
 
-test("should display fallback svg when avatar thumbnail fails to load", async ({ page }) => {
+test("should display CSS thumbnail when avatar thumbnail fails to load", async ({ page }) => {
   // drop all image requests
   await page.route("**/*.{png,jpg,jpeg}*", (route) => route.abort());
 
@@ -16,11 +16,11 @@ test("should display fallback svg when avatar thumbnail fails to load", async ({
   const newCustomersCard = await page.locator(".MuiCard-root", { hasText: /new customers/i });
 
   // chaining, locator find again instead of <>.filter to return multiple elements
-  const fallbackAvatarImages = await newCustomersCard.locator(
-    ".MuiList-root .MuiListItem-root .MuiAvatar-circular svg"
+  const fallbackAvatarCSSThumbnail = await newCustomersCard.locator(
+    ".MuiList-root .MuiListItem-root .MuiAvatar-circular"
   );
 
   // nb: locator.isVisible() does not wait for element to be visible, see: https://github.com/microsoft/playwright/pull/9200
-  await fallbackAvatarImages.first().waitFor();
-  expect(await fallbackAvatarImages.count()).toBeGreaterThan(1);
+  await fallbackAvatarCSSThumbnail.first().waitFor();
+  expect(await fallbackAvatarCSSThumbnail.count()).toBeGreaterThan(1);
 });
